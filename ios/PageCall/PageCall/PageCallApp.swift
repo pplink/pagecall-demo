a7dede493768bb33052ae64bd4ae96e47fc2972c
Sehun Park <defsehun@gmail.com>
2021-07-26 14:32:37 +0900

    init swift UI project

---
+++ ios/PageCall/PageCall/PageCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageCallApp.swift
+//  PageCall
+//
+//  Created by Park Sehun on 2021/07/26.
+//
+
+import SwiftUI
+
+@main
+struct PageCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
