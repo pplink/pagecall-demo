59541213a43d2e0eadb4991aaea906f89d513214
Sehun Park <defsehun@gmail.com>
2021-07-26 17:12:30 +0900

    feat: PageCallDelegate

--- ios/PageCall/PageCall/ViewController.swift
+++ ios/PageCall/PageCall/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         let pageCall = PageCall.sharedInstance()
-        //pageCall.delegate = self
+        pageCall.delegate = self
             
         #if DEBUG
         #else
@@ -31,7 +34,36 @@ class ViewController: UIViewController {
             pageCall.webViewLoadRequest(withURLString: strURL)
         })
     }
-
-
 }
 
+extension ViewController: PageCallDelegate {
+    func pageCallDidClose() {
+        print("pageCallDidClose")
+    }
+
+    func pageCallDidReceive(_ message: WKScriptMessage) {
+        print("pageCallDidReceive message")
+        
+        /* sample JS
+        var message = {
+            command: 'finishedLoading',
+            interval: 1
+        };
+        window.webkit.messageHandlers.pageCallSDK.postMessage(message);
+        */
+        
+        if message.name == "pageCallSDK" {
+            guard let dict = message.body as? [String: AnyObject],
+                  let command = dict["command"] as? String,
+                  let interval = dict["interval"] as? Int else {
+                    return
+            }
+            print("pageCallDidReceiveScriptMessage command: \(command), interval: \(interval)")
+        }
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        print("webView decidePolicyFor navigationAction")
+        decisionHandler(.allow)
+    }
+}
