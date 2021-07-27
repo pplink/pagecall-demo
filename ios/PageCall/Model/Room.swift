785a789815135153d10a9b59548d9ece3ab38f22
Sehun Park <defsehun@gmail.com>
2021-07-27 21:20:05 +0900

    feat: Add struct Room

---
+++ ios/PageCall/Model/Room.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+struct Room: Codable {
+    var id: String
+    var name: String
+    var pcaRoomId: String
+    var start: String?
+    var end: String?
+    var participant: Int?
+    var createdAt: String?
+    var updatedAt: String?
+}
+
+struct Rooms: Codable
+{
+    var liveRooms: [Room]
+    var closedRooms: [Room]
+}
