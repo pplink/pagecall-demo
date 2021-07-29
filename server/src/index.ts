86288e9dff267f7e37674a874d70eb44652d72f4
whitesoil <whitesoil.kr@gmail.com>
2021-07-29 10:30:27 +0900

    ...

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,6 +9,7 @@ function main(port: number) {
 
   app.use((req, res, next) => {
     if (isDisableKeepAlive) {
+      // 서버가 종료 절차에 있을 때, 이 응답을 마지막으로 클라이언트와의 연결을 끊는다.
       res.set('Connection', 'close');
     }
 
@@ -18,11 +19,13 @@ function main(port: number) {
   const server = app.listen(port, () => {
     console.warn(`Listening on port ${port}`);
 
+    // pm2로 실행 시, pm2 클러스터로 실행이 준비 되었다는 시스템콜을 보낸다.
     if (env.NODE_ENV === 'production') {
       process.send('ready');
     }
   });
 
+  // pm2 클러스터로 부터 종료 요청을 받았을 때, 서버를 종료한다.
   process.on('SIGINT', () => {
     isDisableKeepAlive = true;
 
