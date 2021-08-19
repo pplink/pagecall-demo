6f49b007b6c8655c59d53f669dc8c66183857196
whitesoil <whitesoil.kr@gmail.com>
2021-08-19 14:48:01 +0900

    init

---
+++ web/src/env/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Enviromnent Variable Preset for Performance
+ */
+const NODE_ENV = process.env.NODE_ENV;
+
+const SERVER_URL = process.env.SERVER_URL;
+
+const env = {
+  NODE_ENV,
+  SERVER_URL,
+};
+
+export default env;
