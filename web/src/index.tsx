bda2b7e23263809b90398d9eef5061c35bb4236b
whitesoil <whitesoil.kr@gmail.com>
2021-08-23 17:02:29 +0900

    feat: open pagecall in iframe

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import MainPage from './pages/MainPage';
 import { createGlobalStyle } from 'styled-components';
 import { RoomsProvider } from './contexts/RoomsContext';
+import { BrowserRouter, Route } from 'react-router-dom';
+import PageCallPage from './pages/PageCallPage';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -14,7 +16,10 @@ ReactDOM.render(
   <React.StrictMode>
     <RoomsProvider>
       <GlobalStyle />
-      <MainPage />
+      <BrowserRouter>
+        <Route path="/" exact={true} component={MainPage} />
+        <Route path="/room" component={PageCallPage} />
+      </BrowserRouter>
     </RoomsProvider>
   </React.StrictMode>,
   document.getElementById('root'),
