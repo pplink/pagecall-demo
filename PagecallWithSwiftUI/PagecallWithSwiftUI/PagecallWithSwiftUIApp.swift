72b4c534ef7b849f4d9f668324f17b016f52ab1b
Jaeseong Seo <07js23@gmail.com>
2021-11-10 19:53:28 +0900

    SwiftUI: Create a new project

---
+++ PagecallWithSwiftUI/PagecallWithSwiftUI/PagecallWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PagecallWithSwiftUIApp.swift
+//  PagecallWithSwiftUI
+//
+//  Created by Jaeseong Seo on 2021/11/10.
+//
+
+import SwiftUI
+
+@main
+struct PagecallWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
