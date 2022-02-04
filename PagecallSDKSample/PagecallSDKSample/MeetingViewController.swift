325b934f19a21f20fe1226f22bbf2229f1d244c7
Jaeseong Seo <07js23@gmail.com>
2022-02-04 14:48:00 +0900

    Add MeetingViewController

---
+++ PagecallSDKSample/PagecallSDKSample/MeetingViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MeetingViewController: UIViewController {
+    override func viewDidLoad() {
+        print("View did load")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("View did disappear")
+    }
+}
--- PagecallSDKSample/PagecallSDKSample/ViewController.swift
+++ PagecallSDKSample/PagecallSDKSample/ViewController.swift
@@ -1,19 +1,16 @@
-//
-//  ViewController.swift
-//  PagecallSDKSample
-//
-//  Created by Jaeseong Seo on 2022/02/04.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var urlField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let viewController = segue.destination as? MeetingViewController {
+        }
+    }
 }
 
